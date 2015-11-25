64b47edd0255f747db635336eb41696a320631d4
Daniel Lanthier <dlanthier@gmail.com>
2015-11-24 22:33:04 -0500

    Change movies

--- index.ios.js
+++ index.ios.js
@@ -11,6 +11,7 @@ var {
   Text,
   View,
   Image,
+  TouchableHighlight, // Warning! This is IOS only!
 } = React;
 
 var MOCKED_MOVIE_DATA = [
@@ -20,18 +21,41 @@ var MOCKED_MOVIE_DATA = [
     posters: {
       thumbnail: 'http://i.imgur.com/KE3RHWS.jpg'
     }
-  }
+  },
+  {
+    title: 'The Room',
+    year: '2003',
+    posters: {
+      thumbnail: 'http://www.theroommovie.com/roompics/postera.jpg'
+    }
+  },
 ];
-
 var HelloWorld = React.createClass({
+  getInitialState: function() {
+    return {
+      movie: 0,
+    };
+  },
+  buttonClicked: function() {
+    var movieId = this.state.movie == 0 ? 1 :0;
+    this.setState({'movie': movieId});
+  },
   render: function() {
-    var movie = MOCKED_MOVIE_DATA[0];
+    console.log(this.state.movie);
+    return this.renderMovie(MOCKED_MOVIE_DATA[this.state.movie]);
+  },
+  renderMovie: function(movie) {
     return (
       <View style={styles.container}>
         <Text>{movie.title}</Text>
         <Text>{movie.year}</Text>
         <Image style={styles.thumbnail}
           source={{uri: movie.posters.thumbnail}}/>
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this.buttonClicked.bind(this)}>
+            <Text style={styles.buttonText}>Switch Movie</Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -57,6 +81,12 @@ var styles = StyleSheet.create({
   thumbnail: {
     height: 50,
     width: 44,
+  },
+  button: {
+    backgroundColor: '#0266C8',
+  },
+  buttonText: {
+    color: '#FFFFFF',
   },
 });
 
